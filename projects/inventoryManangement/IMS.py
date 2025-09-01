dffa9ea7b4a5241a75bd1c892e0365e5c725af7d
rohitghatal1 <rohitghatal@gmail.com>
2025-09-01 22:10:46 +0545

    added inventroy management file

---
+++ projects/inventoryManangement/IMS.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+def laod_inventory(file = "inventory.csv"):
+    df = pd.read_csv(file)
+    return df_to_nested(df)
+
+def save_inventory(inventory, file="inventory.csv"):
+    df = nested_to_df(inventory)
+    df.to_csv(file, index=False
+              
+              )
+def df_to_nested(df):
+    inventory = {}
+    for _, row in df.iterrows():
+        cat = row["Category"]
+        if cat not in inventory:
+            inventory[cat] = []
+        inventory[cat].append({
+            "ProductID": int(row["ProductID"]),
+            "ProductName": row["ProductName"],
+            "Quantity": int(row["Quantity"]),
+            "Price": float(row["Price"])
+        })
+    return inventory
+
+def nested_to_df(inventory):
+    rows = []
+    for cat, items in inventory.items():
+        for item in items:
+            rows.append([cat, item["ProductID"], item["ProductName"], item["Quantity"], item["Price"]])
+    return pd.DataFrame(rows, columns=["Category", "ProductID", "ProductName", "Quantity", "Price"])
+
+def display_inventory(inventory):
+    print("\n Current Inventory (by Category):\n")
+    for cat, items in inventory.items():
+        print(f"{cat}")
+
+        for item in items:
+            print(f" -{item["ProductName"]} (ID: {item["ProductID"]})" f"Qty: {item['Quantity']} Price: Rs.{item['Price']}")
+    
+    print()
