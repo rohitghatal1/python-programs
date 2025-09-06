fcb99d5c3123cedeed17b0dca8a70b960cb1f51c
rohitghatal1 <rohitghatal@gmail.com>
2025-09-06 13:08:20 +0545

    Updaated

--- learnings/CNN.py
+++ learnings/CNN.py
@@ -1,14 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 
-# 1. Load dataset
 (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
 
-# MNIST images are 28x28 grayscale → reshape to (28,28,1)
 x_train = x_train.reshape((x_train.shape[0], 28, 28, 1)).astype("float32") / 255
 x_test = x_test.reshape((x_test.shape[0], 28, 28, 1)).astype("float32") / 255
 
-# 2. Build CNN model
 model = models.Sequential([
     layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
     layers.MaxPooling2D((2,2)),
@@ -26,7 +23,7 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# 4. Train model
+
 model.fit(x_train, y_train, epochs=5, batch_size=64, validation_split=0.1)
 
 # 5. Evaluate model
--- learnings/list.py
+++ learnings/list.py
@@ -1,6 +1,9 @@
 score = []
+
 for i in range(3):
     score.append(int(input("Enter your score: ")))
 print(score)
+
 updatedScore = [item + 5 if item < 40 else item for item in score]
+
 print(updatedScore)
